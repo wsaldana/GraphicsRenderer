12079581df943746713ea9dae69cd7985f7738ec
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-08-07 21:32:41 -0600

    Draw polygon perimeter

--- sr1.py
+++ sr1.py
@@ -30,26 +30,55 @@ GREEN = color(0,255,0)
 BLUE = color(0,0,255)
 
 class Obj(object):
-  def __init__(self, filename):
-    with open(filename) as f:
-      self.lines = f.read().splitlines()
-    self.vertices = []
-    self.faces = []
-    self.read()
+    def __init__(self, filename):
+        with open(filename) as f:
+            self.lines = f.read().splitlines()
+        self.vertices = []
+        self.faces = []
+        self.read()
 
-  def read(self):
-    for line in self.lines:
-      if line:
-        prefix, value = line.split(' ', 1)
+    def read(self):
+        for line in self.lines:
+            if line:
+                prefix, value = line.split(' ', 1)
 
-        if prefix == 'v':
-          self.vertices.append(
-            list(map(float, value.split(' ')))
-          )
-        elif prefix == 'f':
-          self.faces.append(
-            [list(map(int, face.split('/'))) for face in value.split(' ')]
-          )
+            if prefix == 'v':
+                self.vertices.append(
+                    list(map(float, value.split(' ')))
+                )
+            elif prefix == 'f':
+                self.faces.append(
+                    [list(map(int, face.split('/'))) for face in value.split(' ')]
+                )
+          
+class Polygon(object):
+    def __init__(self, polygon):
+        self.polygon = polygon
+        
+    def normalize(self, n, maximo, minimo):
+        return (n-minimo)/(maximo-minimo)
+                
+    def normalizePolygon(self):
+        values = [c for point in self.polygon for c in point]
+        maximo = max(values)
+        minimo = min(values)
+        normalized = []
+        for point in self.polygon:
+            normalized.append((self.normalize(point[0], maximo, minimo), self.normalize(point[1], maximo, minimo)))
+        self.polygon = normalized
+        
+    def normalizePolygons(self, polygons):
+        #joined = [point for polygon in polygons for point in polygon]
+        values = [c for point in [point for polygon in polygons for point in polygon] for c in point]
+        maximo = max(values)
+        minimo = min(values)
+        normalized = []
+        for polygon in polygons:
+            new_polygon = []
+            for point in polygon:
+                new_polygon.append((self.normalize(point[0], maximo, minimo), self.normalize(point[1], maximo, minimo)))
+            normalized.append(new_polygon)
+        return normalized
 
 class Render(object):
     def __init__(self):
@@ -168,15 +197,6 @@ class Render(object):
             self.framebuffer[ny][nx] = color or self.current_color
 
     def line(self, x0, x1, y0, y1, color=None):
-        if(x0>x1):
-            temp = x0
-            x0 = x1
-            x1 = temp
-        if(y0>y1):
-            temp = y0
-            y0 = y1
-            y1 = temp
-
         pos_viewport_x0 = self.scaleToViewport(x0, 'x')
         pos_viewport_y0 = self.scaleToViewport(y0, 'y')
         pos_viewport_x1 = self.scaleToViewport(x1, 'x')
@@ -188,11 +208,18 @@ class Render(object):
             m = (y1 - y0) / (x1 - x0)
         b = pos_viewport_y1 - (m * pos_viewport_x1)
 
-        if(m <= 1):
+        if(abs(m) <= 1):
+            if(x0>x1) :
+                pos_viewport_x0, pos_viewport_x1 = pos_viewport_x1, pos_viewport_x0
+                pos_viewport_y0, pos_viewport_y1 = pos_viewport_y1, pos_viewport_y0
             for x in range(pos_viewport_x0, pos_viewport_x1):
                 y = round((m * x) + b)
                 self.framebuffer[y][x] = color or self.current_color
         else:
+            if(y0>y1) :
+                pos_viewport_x0, pos_viewport_x1 = pos_viewport_x1, pos_viewport_x0
+                pos_viewport_y0, pos_viewport_y1 = pos_viewport_y1, pos_viewport_y0
+                
             for y in range(pos_viewport_y0, pos_viewport_y1):
                 x = round((y - b) / m)
                 self.framebuffer[y][x] = color or self.current_color
@@ -215,6 +242,15 @@ class Render(object):
                 y2 = (v2[1] + translate[1]) * scale[1]
                 
                 self.line(x1, x2, y1, y2)
+                
+    def drawPolygon(self, polygon, translate=(-0.5,0), scale=(1,1)):
+        n = len(polygon)
+        for i in range(n):
+            x0 = (polygon[i][0] + translate[0]) * scale[0]
+            x1 = (polygon[(i+1)%n][0] + translate[0]) * scale[0]
+            y0 = (polygon[i][1] + translate[1]) * scale[1]
+            y1 = (polygon[(i+1)%n][1] + translate[1]) * scale[1]
+            self.line(x0, x1, y0, y1)
         
 '''
 r = Render(1024, 768)
@@ -257,6 +293,9 @@ class MyGL(object):
 
     def load(self, filename, translate, scale):
         self.render.load(filename, translate, scale)
+        
+    def polygon(self, poly):
+        self.render.drawPolygon(poly)
 
 gl = MyGL()
 gl.glInit()
@@ -269,6 +308,33 @@ gl.glColor(0,1,0)
 #gl.glColor(0,0,0)
 #gl.glLine(0.5, 0.5, -0.5, 0)
 #gl.glColor(1,0,0)
-#gl.glLine(-0.2, -0.4, 0.2, 0.4)
-gl.load("Pokemon.obj", (0, -1), (0.8, 0.8))
+
+#gl.glLine(-0.9, 0.4, -0.1, 0.6)
+#gl.glLine(0.1, 0.6, 0.9, 0.4)
+#gl.glLine(-0.6, -0.1, -0.4, -0.9)
+#gl.glLine(0.4, -0.9, 0.6, -0.1)
+
+#gl.glLine(-0.1, 0.6, -0.9, 0.4)
+#gl.glLine(0.9, 0.4, 0.1, 0.6)
+#gl.glLine(-0.4, -0.9, -0.6, -0.1)
+#gl.glLine(0.6, -0.1, 0.4, -0.9)
+
+#gl.load("Pokemon.obj", (0, -1), (0.8, 0.8))
+
+poly = Polygon(((377, 249), (411, 197), (436, 249)))
+poly.normalizePolygon()
+polys = poly.normalizePolygons((
+    ((165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)),
+    ((321, 335), (288, 286), (339, 251) ,(374, 302)),
+    ((377, 249), (411, 197), (436, 249)),
+    ((413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52), (750, 145), (761, 179), (672, 192) ,(659, 214), (615, 214), (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)),
+    ((682, 175), (708, 120), (735, 148), (739, 170))
+))
+
+#gl.polygon(poly.polygon)
+gl.polygon(polys[0])
+gl.polygon(polys[1])
+gl.polygon(polys[2])
+gl.polygon(polys[3])
+gl.polygon(polys[4])
 gl.glFinish('render')
