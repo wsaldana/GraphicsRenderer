defad3f1e76e517aeb286c6c92422df8bbb7cb23
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-09-09 20:43:34 -0600

    Set colors limits

--- utils.py
+++ utils.py
@@ -13,4 +13,16 @@ def dword(d):
     return struct.pack('=l', d)
 
 def color(r,g,b):
+    if(r>255):
+        r = 255
+    if(g>255):
+        g = 255
+    if(b>255):
+        b = 255
+    if(r<0):
+        r = 0
+    if(g<0):
+        g = 0
+    if(b<0):
+        b = 0
     return bytes([b,g,r])
